f9702e8fcf8f905771a6a5b3ead70dfb07d8d3ad
Daniel Ong <ongfishh@gmail.com>
2022-08-18 00:06:06 +0800

    Add abort controller to API call

--- src/frontend/users/userList.js
+++ src/frontend/users/userList.js
@@ -1,41 +1,51 @@
-import { React, useState, useEffect } from "react";
-import axios from "axios";
+import { React, useState, useEffect } from 'react';
+import axios from 'axios';
 
 const UserList = () => {
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/users/")
-      .then((response) => {
-        setUsers(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+        axios
+            .get('http://localhost:5000/users/', { signal: signal })
+            .then((response) => {
+                setUsers(response.data);
+            })
+            .catch((error) => {
+                if (error.name === 'CanceledError') {
+                    console.error('call to API was cancelled');
+                } else {
+                    console.log(error);
+                }
+            });
 
-  return (
-    <>
-      <div>
-        <h3>Users</h3>
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr>
-                <td key={user._id}>{user.username}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+        return () => {
+            controller.abort();
+        };
+    }, []);
+
+    return (
+        <>
+            <div>
+                <h3>Users</h3>
+                <table className='table'>
+                    <thead className='thead-light'>
+                        <tr>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user._id}>
+                                <td>{user.username}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 };
 
 export default UserList;
