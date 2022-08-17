38877538ed5252fa75c32d511e2713394d8caa1f
zixuan97 <ngzixuan1997@hotmail.com>
2022-08-18 00:58:27 +0800

    Implemented Create for User object

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from "./frontend/NavBar";
-import UserList from "./frontend/users/userList";
+import Navbar from "./frontend/NavBar"
+import UserList from "./frontend/users/userList"
+import CreateUser from "./frontend/users/createUser"
 // import CreateGrade from "./components/edit-exercise.component"
 // import EditUser from "./components/create-exercise.component"
-// import CreateUser from "./components/create-user.component"
+
 
 function App() {
   return (
@@ -16,12 +17,11 @@ function App() {
           <Navbar />
           <br />
           <Routes>
-            {/* <Route path="/" exact component={UserList} /> */}
             <Route path="/" element={<UserList />} />
+            <Route path="/createUser" element={<CreateUser />} />
           </Routes>
 
           {/* <Route path="/edit/:username" component={EditUser} />
-          <Route path="/createUser" component={CreateUser} />
           <Route path="/createGrade" component={CreateGrade} /> */}
         </div>
       </Router>
