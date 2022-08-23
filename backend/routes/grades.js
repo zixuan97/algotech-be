c6400bf87e2355d6f5d9ff282f1f1502e821fd34
Lee Leonard <71783105+leeleonard98@users.noreply.github.com>
2022-08-23 16:52:53 +0800

    Leonard (#3)
    * feat: removed mongoose, mongo db, added prisma and connect to aws rds
    
    * feat: removed mongoose, mongo db, added prisma and connect to aws rds
    
    * feat: removed mongoose, mongo db, added prisma and connect to aws rds
    
    * feat: removed mongoose, mongo db, added prisma and connect to aws rds

--- backend/routes/grades.js
+++ backend/routes/grades.js
@@ -1,28 +1,28 @@
-const router = require('express').Router()
-let Grades = require('../models/grades/grades.model')
+// const router = require('express').Router()
+// let Grades = require('../models/grades/grades.model')
 
-router.route('/').get((req, res) => {
-    Grades.find()
-    .then(grades => res.json(grades))
-    .catch(err => res.status(400).json('Error ' + err))
-})
+// router.route('/').get((req, res) => {
+//     Grades.find()
+//     .then(grades => res.json(grades))
+//     .catch(err => res.status(400).json('Error ' + err))
+// })
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username
-    const mark = Number(req.body.mark)
-    const alphabetGrade = req.body.alphabetGrade
-    const date = Date.parse(req.body.date)
+// router.route('/add').post((req, res) => {
+//     const username = req.body.username
+//     const mark = Number(req.body.mark)
+//     const alphabetGrade = req.body.alphabetGrade
+//     const date = Date.parse(req.body.date)
 
-    const newGrade = new Grades({
-        username,
-        mark,
-        alphabetGrade,
-        date
-    })
+//     const newGrade = new Grades({
+//         username,
+//         mark,
+//         alphabetGrade,
+//         date
+//     })
 
-    newGrade.save()
-    .then(()=> { res.json('Grades Added!')})
-    .catch(err => res.status(400).json('Error ' + err))
-})
+//     newGrade.save()
+//     .then(()=> { res.json('Grades Added!')})
+//     .catch(err => res.status(400).json('Error ' + err))
+// })
 
-module.exports = router
+// module.exports = router
