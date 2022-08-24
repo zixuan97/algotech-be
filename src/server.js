c63e836591c249c32f76f5250d74805a09306ad0
zixuan97 <ngzixuan1997@hotmail.com>
2022-08-24 21:04:46 +0800

    Impl vercel config

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-require('dotenv').config()
-const { Client } = require('pg')
-const express = require('express')
-const cors = require('cors')
-const app = express()
+require('dotenv').config();
+const { Client } = require('pg');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { v4 } = require('uuid');
 
-const userRouter = require('./routes/user')
-const gradesRouter = require('./routes/grades')
+const userRouter = require('./routes/user');
+const gradesRouter = require('./routes/grades');
 
-const port = process.env.PORT || 4000
-app.use(cors())
-app.use(express.json())
+const port = process.env.PORT || 4000;
+app.use(cors());
+app.use(express.json());
 
 const client = new Client({
   user: process.env.RDS_USERNAME,
@@ -17,20 +18,33 @@ const client = new Client({
   database: 'postgres',
   password: process.env.RDS_PASSWORD,
   port: 5432
-})
+});
 
 client.connect(function (err) {
   if (err) {
-    console.error('Database connection failed: ' + err.stack)
-    return
+    console.error('Database connection failed: ' + err.stack);
+    return;
   }
 
-  console.log('Connected to database.')
-})
+  console.log('Connected to database.');
+});
 
-app.use('/users', userRouter)
-// app.use('/grades', gradesRouter);
+app.use('/users', userRouter);
 
 app.listen(port, () => {
-  console.log('Server running on port ' + port)
-})
+  console.log('Server running on port ' + port);
+});
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
