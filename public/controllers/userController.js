a340cf9c0024a1e5fa81965ec52adabd41233a7e
Lee Leonard <71783105+leeleonard98@users.noreply.github.com>
2022-08-28 11:03:58 +0800

    feat: Implemented login with jwt token, added middleware for auth (#16)

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -1,13 +1,47 @@
 const userModel = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const common = require('@kelchy/common');
+const Error = require('../helpers/error');
 
 const create = async (req, res) => {
   const { email, password } = req.body;
+  const { error } = await common.awaitWrap(
+    userModel.create({
+      email,
+      password
+    })
+  );
 
-  const response = await userModel.create({
-    email,
-    password
+  if (error) {
+    res.json(Error.http(error));
+  } else {
+    return res.json({ message: 'User created' });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findUserByEmail({
+    email
   });
-  return res.json({ message: 'User created', response });
+  console.log(user);
+  if (user && (await bcrypt.compare(password, user.password))) {
+    // Create token
+    const token = jwt.sign({ user_id: user.id, email }, process.env.TOKEN_KEY, {
+      expiresIn: '2h'
+    });
+
+    // save user token
+    user.token = token;
+
+    // user
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('Invalid Credentials');
+  }
 };
 
 exports.create = create;
+exports.login = login;
--- public/routes/userRoutes.js
+++ public/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const UserController = require('../controllers/userController');
+const auth = require('../middleware/auth');
 
 router.post('/', UserController.create);
+router.post('/login', UserController.login);
 
 module.exports = router;
