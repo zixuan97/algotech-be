6ecc2e1e84121d7fd04a3ac71365d5fd4dd3c789
merylseow <71954278+merylseow@users.noreply.github.com>
2022-09-03 12:30:59 +0800

    CRUD for user + enable/disable user + change user roles

--- public/models/userModel.js
+++ public/models/userModel.js
@@ -1,8 +1,8 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
 const common = require('@kelchy/common');
-
-const prisma = new PrismaClient();
+const prisma = new PrismaClient(); 
+const { UserStatus, UserRole } = require('@prisma/client');
 
 const create = async (req) => {
     const { email, password } = req;
@@ -19,6 +19,11 @@ const create = async (req) => {
     if (error) {
         throw error;
     }
+};
+
+const getUsers = async () => {
+    const users = await prisma.User.findMany({});
+    return users;
 };
 
 const findUserById = async (req) => {
@@ -41,6 +46,92 @@ const findUserByEmail = async (req) => {
     return user;
 };
 
+const getUserDetails = async (req) => {
+    const { id } = req;
+    const user = await prisma.User.findUnique({
+        where: {
+            id: Number(id)
+        }
+    });
+    return user;
+};
+
+const editUser = async (req) => {
+    const { updatedUser } = req;
+    const id = updatedUser.id;
+    user = await prisma.User.update({
+        where: { id: Number(id) },
+        data: {
+            email: updatedUser.email,
+            password: updatedUser.password,
+            role: updatedUser.role,
+            status: updatedUser.status,
+        },
+    });
+    return user;
+};
+
+const deleteUserById = async (req) => {
+    const { id } = req;
+    const user = await prisma.User.delete({
+        where: {
+            id: Number(id)
+        }
+    });
+    return id;
+};
+
+const enableUser = async (req) => {
+    const { id } = req;
+    const user = await prisma.User.update({
+        where: { id: Number(id) },
+        data: {
+            status: UserStatus.ACTIVE,
+        },
+    });
+    return user;
+};
+
+const disableUser = async (req) => {
+    const { id } = req;
+    const user = await prisma.User.update({
+        where: { id: Number(id) },
+        data: {
+            status: UserStatus.DISABLED,
+        },
+    });
+    return user;
+};
+
+const changeUserRole = async (req) => {
+    const { id, action } = req;
+    if (action == 'intern') {
+        newRole = UserRole.INTERN;
+    } else if (action === 'pt') {
+        newRole = UserRole.PARTTIME;
+    } else if (action === 'ft') {
+        newRole = UserRole.FULLTIME;
+    } else if (action === 'customer') {
+        newRole = UserRole.CUSTOMER;
+    } else if (action === 'admin') {
+        newRole = UserRole.ADMIN;
+    }
+    const user = await prisma.User.update({
+        where: { id: Number(id) },
+        data: {
+            role: newRole
+        },
+    });
+    return user;
+};
+
 exports.create = create;
+exports.getUsers = getUsers;
 exports.findUserById = findUserById;
 exports.findUserByEmail = findUserByEmail;
+exports.getUserDetails = getUserDetails;
+exports.editUser = editUser;
+exports.deleteUserById = deleteUserById;
+exports.enableUser = enableUser;
+exports.disableUser = disableUser;
+exports.changeUserRole = changeUserRole;
