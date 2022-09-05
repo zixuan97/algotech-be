33da558725c0b8c87e1ff33da99d0c2db8f2ea55
Lee Leonard <71783105+leeleonard98@users.noreply.github.com>
2022-09-05 15:15:51 +0800

    AT-75: Basic Functionalities for IMS (#20)
    * feat(category) : category routes and methods
    
    * feat(category): added delete routes
    
    * fix(category): fix category naming
    
    * fix(category): fix delete method

--- public/controllers/categoryController.js
+++ public/controllers/categoryController.js
@@ -18,25 +18,44 @@ const create = async (req, res) => {
   }
 };
 
-const getAllcategorys = async (req, res) => {
+const getAllCategories = async (req, res) => {
   const { data, error } = await common.awaitWrap(
-    categoryModel.getAllcategorys({})
+    categoryModel.getAllcategories({})
   );
 
   if (error) {
     res.json(Error.http(error));
   } else {
-    res.json({ data, message: 'Retrieved all categorys' });
+    res.json({ data, message: 'Retrieved all categories' });
   }
 };
 
 const updateCategory = async (req, res) => {
-  const { name, description } = req.body;
+  const { id, name, description } = req.body;
   const { data, error } = await common.awaitWrap(
-    categoryModel.updateCategory({ name, description })
+    categoryModel.updateCategory({ id, name, description })
   );
+  if (error) {
+    res.json(Error.http(error));
+  } else {
+    res.json({ data, message: 'Retrieved all categories' });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  const { id } = req.body;
+  console.log('hello');
+  const { error } = await common.awaitWrap(
+    categoryModel.deleteCategory({ id })
+  );
+  if (error) {
+    res.json(Error.http(error));
+  } else {
+    res.json({ message: `Deleted category with id ${id}` });
+  }
 };
 
 exports.create = create;
-exports.getAllcategorys = getAllcategorys;
+exports.getAllCategories = getAllCategories;
 exports.updateCategory = updateCategory;
+exports.deleteCategory = deleteCategory;
