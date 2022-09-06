d6b7ae9809371de39584ae15cf34f57267e9e47e
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2022-09-06 15:52:04 +0800

    fix(email) : change email

--- public/models/userModel.js
+++ public/models/userModel.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
-const prisma = new PrismaClient(); 
+const prisma = new PrismaClient();
 const { UserStatus, UserRole } = require('@prisma/client');
 const nodemailer = require('nodemailer');
 
@@ -42,107 +42,107 @@ const findUserByEmail = async (req) => {
 };
 
 const getUserDetails = async (req) => {
-    const { id } = req;
-    const user = await prisma.User.findUnique({
-        where: {
-            id: Number(id)
-        }
-    });
-    return user;
+  const { id } = req;
+  const user = await prisma.User.findUnique({
+    where: {
+      id: Number(id)
+    }
+  });
+  return user;
 };
 
 const editUser = async (req) => {
-    const { updatedUser } = req;
-    const id = updatedUser.id;
-    user = await prisma.User.update({
-        where: { id: Number(id) },
-        data: {
-            email: updatedUser.email,
-            password: updatedUser.password,
-            role: updatedUser.role,
-            status: updatedUser.status,
-        },
-    });
-    return user;
+  const { updatedUser } = req;
+  const id = updatedUser.id;
+  user = await prisma.User.update({
+    where: { id: Number(id) },
+    data: {
+      email: updatedUser.email,
+      password: updatedUser.password,
+      role: updatedUser.role,
+      status: updatedUser.status
+    }
+  });
+  return user;
 };
 
 const deleteUserById = async (req) => {
-    const { id } = req;
-    const user = await prisma.User.delete({
-        where: {
-            id: Number(id)
-        }
-    });
-    return id;
+  const { id } = req;
+  const user = await prisma.User.delete({
+    where: {
+      id: Number(id)
+    }
+  });
+  return id;
 };
 
 const enableUser = async (req) => {
-    const { id } = req;
-    const user = await prisma.User.update({
-        where: { id: Number(id) },
-        data: {
-            status: UserStatus.ACTIVE,
-        },
-    });
-    return user;
+  const { id } = req;
+  const user = await prisma.User.update({
+    where: { id: Number(id) },
+    data: {
+      status: UserStatus.ACTIVE
+    }
+  });
+  return user;
 };
 
 const disableUser = async (req) => {
-    const { id } = req;
-    const user = await prisma.User.update({
-        where: { id: Number(id) },
-        data: {
-            status: UserStatus.DISABLED,
-        },
-    });
-    return user;
+  const { id } = req;
+  const user = await prisma.User.update({
+    where: { id: Number(id) },
+    data: {
+      status: UserStatus.DISABLED
+    }
+  });
+  return user;
 };
 
 const changeUserRole = async (req) => {
-    const { id, action } = req;
-    if (action == 'intern') {
-        newRole = UserRole.INTERN;
-    } else if (action === 'pt') {
-        newRole = UserRole.PARTTIME;
-    } else if (action === 'ft') {
-        newRole = UserRole.FULLTIME;
-    } else if (action === 'customer') {
-        newRole = UserRole.CUSTOMER;
-    } else if (action === 'admin') {
-        newRole = UserRole.ADMIN;
+  const { id, action } = req;
+  if (action == 'intern') {
+    newRole = UserRole.INTERN;
+  } else if (action === 'pt') {
+    newRole = UserRole.PARTTIME;
+  } else if (action === 'ft') {
+    newRole = UserRole.FULLTIME;
+  } else if (action === 'customer') {
+    newRole = UserRole.CUSTOMER;
+  } else if (action === 'admin') {
+    newRole = UserRole.ADMIN;
+  }
+  const user = await prisma.User.update({
+    where: { id: Number(id) },
+    data: {
+      role: newRole
     }
-    const user = await prisma.User.update({
-        where: { id: Number(id) },
-        data: {
-            role: newRole
-        },
-    });
-    return user;
+  });
+  return user;
 };
 
 const sendEmail = async (req) => {
-    const { email } = req;
-    const transporter = nodemailer.createTransport({
-        host: "outlook.office365.com", // change host according to email type (outlook/ gmail) of sender
-        port: 587,
-        secure: false,
-        auth: {
-          user: 'e0421281@u.nus.edu', // change email accordingly
-          pass: process.env.PASSWORD, // set password in .env
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-    const info = await transporter.sendMail({
-        from: '"Meryl" <e0421281@u.nus.edu>', // change email accordingly
-        to: email,
-        subject: "Hello ✔",
-        text: "Hello world?",
-        html: "<b>Send a link here which directs user to a form that lets them change pw, then use edit endpoint to update</b>",
-    });
-    console.log("Message sent");    
-}
+  const { email } = req;
+  const transporter = nodemailer.createTransport({
+    host: 'outlook.office365.com', // change host according to email type (outlook/ gmail) of sender
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'exleolee@gmail.com', // change email accordingly
+      pass: process.env.PASSWORD // set password in .env
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+  const info = await transporter.sendMail({
+    from: '"Meryl" <e0421281@u.nus.edu>', // change email accordingly
+    to: email,
+    subject: 'Hello ✔',
+    text: 'Hello world?',
+    html: '<b>Send a link here which directs user to a form that lets them change pw, then use edit endpoint to update</b>'
+  });
+  console.log('Message sent');
+};
 
 exports.createUser = createUser;
 exports.getUsers = getUsers;
