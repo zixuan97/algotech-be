440286e9c3fc7292e213b6ef2187ff150805585f
zixuan97 <ngzixuan1997@hotmail.com>
2022-09-07 22:03:44 +0800

    Auth cookie fix(4)

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -83,7 +83,7 @@ const auth = async (req, res) => {
             domain:
               process.env.NODE_ENV === 'development'
                 ? 'localhost'
-                : '.legendkong.com/'
+                : '.legendkong.com'
           })
           .json({
             data: user,
