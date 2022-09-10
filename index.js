2f8916e8f5d16fd2a5e5ffae0f0b0cd3816c0165
Weng Jia Jun <e0406810@u.nus.edu>
2022-09-10 13:37:26 +0800

    feat: implemented brand routes

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.get('/', (req, res) => {
 app.use('/user', require('./public/routes/userRoutes'));
 app.use('/category', require('./public/routes/categoryRoutes'));
 app.use('/product', require('./public/routes/productRoutes'));
+app.use('/brand', require('./public/routes/brandRoutes'));
 
 const port = process.env.PORT || 4000;
 
