3b093c0fe69d00f877ded9767a5ca6799635b01c
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2022-09-11 00:47:34 +0800

    feat: added pdf generator

--- public/helpers/pdf.js
+++ public/helpers/pdf.js
@@ -1,45 +1,61 @@
-const chromium = require('chrome-aws-lambda');
-const common = require('@kelchy/common');
-const { puppeteer } = require('chrome-aws-lambda');
+// const chromium = require('chrome-aws-lambda');
+// const common = require('@kelchy/common');
+// const { puppeteer } = require('chrome-aws-lambda');
+const getStream = require('get-stream');
+const PDFDocument = require('pdfkit');
 
-const generatePdfTemplate = async (html = '') => {
-  const { data: browser, error: puppeteerError } = await common.awaitWrap(
-    chromium.puppeteer.launch({
-      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true
-    })
-  );
+// const generatePdfTemplate = async (html = '') => {
+//   const { data: browser, error: puppeteerError } = await common.awaitWrap(
+//     chromium.puppeteer.launch({
+//       args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+//       defaultViewport: chromium.defaultViewport,
+//       executablePath: await chromium.executablePath,
+//       headless: true,
+//       ignoreHTTPSErrors: true
+//     })
+//   );
 
-  if (puppeteerError) {
-    console.log(puppeteer);
-    throw new Error('Error launching puppeteer');
-  }
-  try {
-    const { data: page, error: newPageError } = await common.awaitWrap(
-      browser.newPage()
-    );
-    if (newPageError) {
-      throw new Error('Error opening new page');
-    }
-    const { error: setContentError } = await common.awaitWrap(
-      page.setContent(html)
-    );
-    if (setContentError) {
-      throw new Error('Error setting content');
-    }
-    const { data: pdfBuffer, error: generatePdfError } = await common.awaitWrap(
-      page.pdf()
-    );
-    if (generatePdfError) {
-      throw new Error('Error generating pdf');
-    }
-    return pdfBuffer;
-  } finally {
-    await browser.close();
-  }
+//   if (puppeteerError) {
+//     console.log(puppeteer);
+//     throw new Error('Error launching puppeteer');
+//   }
+//   try {
+//     const { data: page, error: newPageError } = await common.awaitWrap(
+//       browser.newPage()
+//     );
+//     if (newPageError) {
+//       throw new Error('Error opening new page');
+//     }
+//     const { error: setContentError } = await common.awaitWrap(
+//       page.setContent(html)
+//     );
+//     if (setContentError) {
+//       throw new Error('Error setting content');
+//     }
+//     const { data: pdfBuffer, error: generatePdfError } = await common.awaitWrap(
+//       page.pdf()
+//     );
+//     if (generatePdfError) {
+//       throw new Error('Error generating pdf');
+//     }
+//     return pdfBuffer;
+//   } finally {
+//     await browser.close();
+//   }
+// };
+
+const generatePdfTemplate = async () => {
+  // Create a document
+  const doc = new PDFDocument({ bufferPages: true });
+
+  // Add an image, constrain it to a given size, and center it vertically and horizontally
+
+  // Add another page
+  doc.addPage().fontSize(25).text('Here is some vector graphics...', 100, 100);
+
+  // Finalize PDF file
+  doc.end();
+  return await getStream.buffer(doc);
 };
 
 exports.generatePdfTemplate = generatePdfTemplate;
