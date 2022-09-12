2db6ec88ac143aa2242ef9f420e869c5a23ea664
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2022-09-12 14:39:07 +0800

    fix: reset migrations

--- prisma/migrations/20220905182422_remove/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `available_stocks` on the `Product` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Product" DROP COLUMN "available_stocks";
