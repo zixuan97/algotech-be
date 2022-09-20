fdef27c1944eef74c4ffcb5562f1c7194d1a2fc8
tengzikun <meleenoob971@gmail.com>
2022-09-20 17:19:41 +0800

    feat: view bundle

--- public/routes/productRoutes.js
+++ public/routes/productRoutes.js
@@ -9,6 +9,7 @@ router.get('/sku/:sku', productController.getProductBySku);
 router.get('/category/:categoryId', productController.getAllProductsByCategory);
 router.get('/location/:locationId', productController.getAllProductsByLocation);
 router.get('/brand/:brandId', productController.getAllProductsByBrand);
+router.get('/bundle/:bundleId', productController.getAllProductsByBundle);
 router.put('/', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
 router.post('/excel', productController.generateExcel);
