6af23fb123fe1507b07f4731d6e4bdecacb9bc09
Daniel Ong <ongfishh@gmail.com>
2022-09-30 00:55:45 +0800

    shift back pusher initialisation to global

--- public/utils/pusherUtil.js
+++ public/utils/pusherUtil.js
@@ -1,13 +1,14 @@
 const Pusher = require('pusher');
 
+const pusher = new Pusher({
+  appId: '1483851',
+  key: '33f7d7f44d38ff91c104',
+  secret: '55aef9641dfa93e130e5',
+  cluster: 'ap1',
+  useTLS: true
+});
+
 const sendPusherMsg = (data) => {
-  const pusher = new Pusher({
-    appId: '1483851',
-    key: '33f7d7f44d38ff91c104',
-    secret: '55aef9641dfa93e130e5',
-    cluster: 'ap1',
-    useTLS: true
-  });
   pusher.trigger('algotech-pusher', 'shopify-webhook', {
     data: JSON.stringify(data)
   });
