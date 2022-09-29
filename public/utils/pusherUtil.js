764f75c8451ad5512f2161c8549c386f2d9afbc2
Daniel Ong <ongfishh@gmail.com>
2022-09-30 00:31:23 +0800

    update pusher util logic

--- public/utils/pusherUtil.js
+++ public/utils/pusherUtil.js
@@ -1,16 +1,16 @@
 const Pusher = require('pusher');
 
-const pusher = new Pusher({
-  appId: '1483851',
-  key: '33f7d7f44d38ff91c104',
-  secret: '55aef9641dfa93e130e5',
-  cluster: 'ap1',
-  useTLS: true
-});
-
-const sendPusherMsg = (data) =>
+const sendPusherMsg = (data) => {
+  const pusher = new Pusher({
+    appId: '1483851',
+    key: '33f7d7f44d38ff91c104',
+    secret: '55aef9641dfa93e130e5',
+    cluster: 'ap1',
+    useTLS: true
+  });
   pusher.trigger('algotech-pusher', 'shopify-webhook', {
     data: JSON.stringify(data)
   });
+};
 
 exports.sendPusherMsg = sendPusherMsg;
