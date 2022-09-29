b13c0135a499f25ff9547c7dcd51f0d9313b5932
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2022-09-29 20:10:42 +0800

    fix: routes

--- public/routes/shopifyRoutes.js
+++ public/routes/shopifyRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const cors = require('cors');
 const shopifyController = require('../controllers/shopifyController');
 
-router.get('/orders', shopifyController.addShopifyOrders);
+router.post('/orders', shopifyController.addShopifyOrders);
 router.post('/', shopifyController.createOrderWebhook);
 router.get('/webhook', cors(), shopifyController.sendOrderWebhook);
 module.exports = router;
