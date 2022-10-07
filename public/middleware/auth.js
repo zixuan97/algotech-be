34242d65dc8a6b39ff138ef68f1bed86663988f2
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2022-10-07 11:43:32 +0800

    fix: middleware test

--- public/middleware/auth.js
+++ public/middleware/auth.js
@@ -34,15 +34,15 @@ const whiteListInternal = (req, res, next) => {
     'https://algotech-fe.vercel.app',
     'https://algotech-fe-prod.vercel.app'
   ];
-  //console.log(req.headers.origin);
-  //console.log(req.hostname);
+  console.log(req.headers.origin);
+  console.log(req.hostname);
   if (
     corsWhitelist.includes(req.headers.origin) |
     (req.hostname == 'localhost')
   ) {
     return next();
   } else {
-    return res.status(401).send('Unauthorized');
+    return res.status(401).send({ message: 'Unauthorized' });
   }
 };
 
