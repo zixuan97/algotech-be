b55c9b4daa0863e39f3cf7a53537ed7e00da4fb9
merylseow <71954278+merylseow@users.noreply.github.com>
2022-10-11 09:39:10 +0800

    newsletter methods

--- public/controllers/newsletterController.js
+++ public/controllers/newsletterController.js
@@ -2,19 +2,13 @@ const newsletterModel = require('../models/newsletterModel');
 const common = require('@kelchy/common');
 const Error = require('../helpers/error');
 const { log } = require('../helpers/logger');
+const fs = require("fs");
 
 const createNewsletter = async (req, res) => {
-  const {
-    emailDate,
-    name,
-    emailSubject,
-    emailBodyTitle,
-    emailBody,
-    discountCode
-  } = req.body;
+  const { emailDate, name, emailSubject, emailBodyTitle, emailBody, discountCode } = req.body;
   const { data, error } = await common.awaitWrap(
     newsletterModel.createNewsletter({
-      emailDate,
+      emailDate: new Date(emailDate),
       name,
       emailSubject,
       emailBodyTitle,
@@ -43,7 +37,7 @@ const getAllNewsletters = async (req, res) => {
     res.json(data);
   }
 };
-
+  
 const getNewsletter = async (req, res) => {
   try {
     const { id } = req.params;
@@ -55,12 +49,52 @@ const getNewsletter = async (req, res) => {
     res.status(500).send('Server Error');
   }
 };
+
+const updateNewsletter = async (req, res) => {
+  const { id, name, emailSubject, emailBodyTitle, emailBody, discountCode } = req.body;
+  const { data, error } = await common.awaitWrap(
+    newsletterModel.updateNewsletter({ id, name, emailSubject, emailBodyTitle, emailBody, discountCode })
+  );
+  if (error) {
+    log.error('ERR_NEWSLETTER_UPDATE_NEWSLETTER', error.message);
+    const e = Error.http(error);
+    res.status(e.code).json(e.message);
+  } else {
+    log.out('OK_NEWSLETTER_UPDATE_NEWSLETTER');
+    res.json(data);
+  }
+};
+
+const deleteNewsletter = async (req, res) => {
+  const { id } = req.params;
+  const { error } = await common.awaitWrap(
+    newsletterModel.deleteNewsletter({ id })
+  );
+  if (error) {
+    log.error('ERR_NEWSLETTER_DELETE_NEWSLETTER', error.message);
+    const e = Error.http(error);
+    res.status(e.code).json(e.message);
+  } else {
+    log.out('OK_NEWSLETTER_DELETE_NEWSLETTER');
+    res.json({ message: `Deleted newsletter with id:${id}` });
+  }
+};
+
 const generateNewsletterHtml = async (req, res) => {
+  const { title, contentBody, discountCode } = req.body;
   const path = process.cwd() + '/public/newsletter.html';
-  res.sendFile(path);
+  const temp = process.cwd() + '/public/temp.html';
+  let content = fs.readFileSync(path, 'utf8');
+  content = content.replace("sample", title);
+  content = content.replace("placeholder", contentBody);
+  content = content.replace("test", discountCode);
+  fs.writeFileSync(temp, content, 'utf-8');
+  res.sendFile(temp);
 };
 
 exports.createNewsletter = createNewsletter;
 exports.getAllNewsletters = getAllNewsletters;
 exports.getNewsletter = getNewsletter;
+exports.updateNewsletter = updateNewsletter;
+exports.deleteNewsletter = deleteNewsletter;
 exports.generateNewsletterHtml = generateNewsletterHtml;
--- public/routes/newsletterRoutes.js
+++ public/routes/newsletterRoutes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const newsletterController = require('../controllers/newsletterController');
 
 router.post('/', newsletterController.createNewsletter);
-router.get('/id/:id', newsletterController.getNewsletter);
+router.get('/:id', newsletterController.getNewsletter);
 router.get('/all', newsletterController.getAllNewsletters);
 router.get('/template', newsletterController.generateNewsletterHtml);
+
 module.exports = router;
